eafb0cbcbab2a15661795167a4e0c9bfccc3f886
oddlama <oddlama@oddlama.org>
2025-05-03 19:27:09 +0200

    feat: add basic clusters without attributes

---
+++ src/types.rs
@@ -0,0 +1,71 @@
+macro_rules! basic_type {
+    ($name: ident, $type: ty, $non: expr, $id: literal, $len: literal) => {
+        pub struct $name($type);
+        impl super::ZCLType for $name {
+            type T = $type;
+
+            const NON_VALUE: Option<Self::T> = $non;
+
+            const ID: u8 = $id;
+
+            fn len(&self) -> usize {
+                $len
+            }
+        }
+    };
+}
+basic_type!(Unknown, (), None, 0xff, 0x0);
+basic_type!(NoData, (), None, 0x00, 0x0);
+
+basic_type!(Data8, [u8; 1], None, 0x08, 0x1);
+basic_type!(Data16, [u8; 2], None, 0x09, 0x2);
+basic_type!(Data24, [u8; 3], None, 0x0a, 0x3);
+basic_type!(Data32, [u8; 4], None, 0x0b, 0x4);
+basic_type!(Data40, [u8; 5], None, 0x0c, 0x5);
+basic_type!(Data48, [u8; 6], None, 0x0d, 0x6);
+basic_type!(Data56, [u8; 7], None, 0x0e, 0x7);
+basic_type!(Data64, [u8; 8], None, 0x0f, 0x8);
+
+basic_type!(Boolean, Option<bool>, Some(None), 0x10, 0x1);
+
+basic_type!(Bitmap8, u8, None, 0x18, 0x1);
+
+basic_type!(U8, u8, Some(0xff), 0x20, 0x1);
+basic_type!(U16, u16, Some(0xffff), 0x21, 0x2);
+basic_type!(U24, u32, Some(0xffffff), 0x22, 0x3);
+
+basic_type!(I8, i8, Some(0x80), 0x28, 0x1);
+basic_type!(I16, i16, Some(0x8000), 0x29, 0x2);
+
+pub trait ZCLEnum {
+    const NON_VALUE: Self;
+}
+pub struct Enum8<T: ZCLEnum>(T);
+impl<T: ZCLEnum> super::ZCLType for Enum8<T> {
+    type T = T;
+    const NON_VALUE: Option<Self::T> = Some(T::NON_VALUE);
+    const ID: u8 = 0x30;
+
+    fn len(&self) -> usize {
+        0x1
+    }
+}
+
+basic_type!(F16, f16, Some(f16::NAN), 0x38, 0x2);
+basic_type!(F32, f32, Some(f32::NAN), 0x38, 0x2);
+
+pub struct CharacterString<'a>(Option<&'a str>);
+impl<'a> super::ZCLType for CharacterString<'a> {
+    type T = Option<&'a str>;
+
+    const NON_VALUE: Option<Self::T> = Some(None);
+
+    const ID: u8 = 0x42;
+
+    fn len(&self) -> usize {
+        match self.0 {
+            Some(x) => x.len() + 1,
+            None => 1,
+        }
+    }
+}
